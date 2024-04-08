e124f15d703a9f5236080b0b39290ee7319c7e26
ViktorBusk <bviktor228@gmail.com>
2024-04-09 00:30:23 +0200

    Updated cmp and which-key

--- init.lua
+++ init.lua
@@ -437,10 +437,76 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local which_key = require 'which-key'
 
-      -- Document existing key chains
-      require('which-key').register {
+      which_key.setup {
+        plugins = {
+          marks = true, -- shows a list of your marks on ' and `
+          registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+          spelling = {
+            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            suggestions = 20, -- how many suggestions should be shown in the list?
+          },
+          -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+          -- No actual key bindings are created
+          presets = {
+            operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            motions = true, -- adds help for motions
+            text_objects = true, -- help for text objects triggered after entering an operator
+            windows = true, -- default bindings on <c-w>
+            nav = true, -- misc bindings to work with windows
+            z = false, -- bindings for folds, spelling and others prefixed with z
+            g = false, -- bindings for prefixed with g
+          },
+        },
+        -- add operators that will trigger motion and text object completion
+        -- to enable all native operators, set the preset / operators plugin above
+        -- operators = { gc = "Comments" },
+        key_labels = {
+          -- override the label used to display some keys. It doesn't effect WK in any other way.
+          -- For example:
+          -- ["<space>"] = "SPC",
+          -- ["<cr>"] = "RET",
+          -- ["<tab>"] = "TAB",
+        },
+        icons = {
+          breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
+          separator = '➜', -- symbol used between a key and it's label
+          group = '+', -- symbol prepended to a group
+        },
+        popup_mappings = {
+          scroll_down = '<c-d>', -- binding to scroll down inside the popup
+          scroll_up = '<c-u>', -- binding to scroll up inside the popup
+        },
+        window = {
+          border = 'none', -- none, single, double, shadow
+          position = 'bottom', -- bottom, top
+          margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+          padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+          winblend = 0,
+        },
+        layout = {
+          height = { min = 4, max = 25 }, -- min and max height of the columns
+          width = { min = 20, max = 50 }, -- min and max width of the columns
+          spacing = 3, -- spacing between columns
+          align = 'left', -- align columns left, center or right
+        },
+        ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+        hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
+        show_help = true, -- show help message on the command line when the popup is visible
+        triggers = 'auto', -- automatically setup triggers
+        -- triggers = {"<leader>"} -- or specify a list manually
+        triggers_blacklist = {
+          -- list of mode / prefixes that should never be hooked by WhichKey
+          -- this is mostly relevant for key maps that start with a native binding
+          -- most people should not need to change this
+          i = { 'j', 'k' },
+          v = { 'j', 'k' },
+        },
+      }
+      -- Document e
+      -- xisting key chains
+      which_key.register {
         -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
         -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -891,7 +957,7 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = 'menu,menuone,noinsert,noselect' },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
@@ -1055,7 +1121,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = true },
+      indent = { enable = true, disable = { 'lua', 'python', 'cpp', 'c', 'ruby' } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
